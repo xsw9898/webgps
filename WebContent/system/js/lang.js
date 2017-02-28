2abae85fbf103f044b63df74c406479e11da9747
xsw9898 <xushaowei_98@163.com>
2017-02-28 11:51:36 +0800

    提交文件 git commit -m 提交文件 git commit -m 提交文件

---
+++ WebContent/system/js/lang.js
@@ -0,0 +1,412 @@
+﻿function langChinese(){
+	this.chinese = "中文";
+	this.english = "English";
+	this.msgTitle = "提示信息";
+	this.login = "登 录";
+	this.sysTitle = "GPS 车辆管理后台系统";
+	this.IDNO = "编号";
+	this.operator = "操作";
+	this.status = "状态";
+	this.online = "在线";
+	this.offline = "离线";
+	this.search = "搜 索";
+	this.searchDevice = "搜索终端";
+	this.searchClient = "搜索客户";
+	this.save = "保 存";
+	this.more = "更多";
+	this.all = "全部";
+	this.view = "查看";
+	this.loading = "正在加载...";
+	this.saving = "正在保存...";
+	this.importing = "正在导入...";
+	this.addok = "添加成功！";
+	this.saveok = "保存成功！";
+	this.importok = "导入成功！";
+	this.deleteok = "删除成功！";
+	this.deleting = "正在执行删除操作...";
+	this.index = "序号";
+	this.selectAll = "全选";
+	this.query = "查 询";
+	this.begintime = "开始时间";
+	this.endtime = "结束时间";
+	this.labelBegintime = "开始时间:";
+	this.labelEndtime = "结束时间:";
+	this.content = "内容";
+	this.time = "时间";
+	this.username = "用户名";
+	this.server = "服务器";
+	this.prevPage = "上一页";
+	this.nextPage = "下一页";
+	this.total = "共";
+	this.page = "页";
+	this.record = "条记录";
+	this.current = "第";
+	this.go = "跳";
+	this.hasReachedHome = "已经到达首页！";
+	this.hasReachedEnd = "已经到达末页！";
+	this.pageOverRange = "跳转页码已经超过总页数！";
+	this.pageZoneUnvalid = "跳转页码不得小于1！";
+	this.add = "添加";
+	this.edit = "编辑";
+	this.del = "删除";
+	this.addSel = "添加所选";
+	this.delSel = "删除所选";
+	this.adding = "正在添加中...";
+	this.addSuc = "添加成功";
+	this.delconfirm = "数据删除后将无法还原！是否删除？";
+	this.terminalMobile = "人员终端";
+	this.terminalVehicle = "车载终端";
+	this.terminalDvr = "DVS终端";
+	this.terminalPad = "Pad终端";
+	this.yes = "是";
+	this.no = "否";
+		
+	//登录页面
+	this.login_usrLogin = "用户登录";
+	this.login_UserName = "用户名";	
+	this.login_Password = "密码";
+	this.login_FlashVerifycode = "点击图片刷新验证码";
+	this.login_UserNameEmpty = "用户名不得为空！";
+	this.login_VerifycodeEmpty = "校验码不得为空！";
+	this.login_VerifycodeLength = "校验码长度不足4位！";
+	this.login_InputUserName = "请输入用户名";	
+	this.login_logining = "正在登录，请等待...";
+	
+	//-----------------------------首页词条-----------------------------//
+	this.home_welcome = "欢迎您！";
+	this.home_title = "车联网监控管理平台";
+	this.home_config = "设置";
+	this.home_exit = "退出";
+	this.home_roleAdmin = "系统管理员";
+	this.home_roleUser = "普通用户";
+	this.home_navHome ="首页";
+	this.home_navDevice ="终端管理";
+	this.home_navClient ="客户管理";
+	this.home_navStatus ="在线状态";
+	this.home_navServer ="服务器管理";
+	this.home_navLog ="日志查询";
+	this.home_navAdAndNews ="广告消息管理";
+	this.home_searchDevice = "请输入终端名称或编号";
+	this.home_searchClient = "请输入客户名称或帐号";
+	this.home_exitTip = "正在注销...";
+	this.home_searchDevIdno = "请输入终端编号";
+	//修改密码
+	this.home_changePassword = "修改密码";
+	this.home_lablePwd = "旧密码：";
+	this.home_lableNew = "新密码：";
+	this.home_lableConfirmPwd = "密码确认：";
+	this.home_errConfirmPwd = "与新密码不一致！";
+	this.home_changeAccount = "我的帐号";
+	//
+	this.home_statDevice = "终端信息";
+	this.home_deviceManageCount = "系统管理终端总数：";
+	this.home_deviceTotalCount = "终端总数：";
+	this.home_deviceStoreCount = "库存数目：";
+	this.home_deviceOnlineCount = "在线数目：";
+	
+	this.home_statClient = "客户信息";
+	this.home_clientTotalCount = "客户总数：";
+	this.home_clientUserCount = "用户数目：";
+	this.home_clientOnlineCount = "在线客户端数目：";
+	
+	this.home_statServer = "服务器信息";
+	this.home_serverLogin = "登录服务器：";
+	this.home_serverCount = "业务服务器数目：";
+	this.home_serverOnlineCount = "在线数目：";
+	
+	//-------------------------------设备管理-------------------------------/
+	this.device_title = "终端管理";
+	this.device_all = "所有终端";
+	this.device_store = "库存终端";
+	this.device_7dayexpire = "7天后欠费终端";
+	this.device_30dayexpire = "30天后欠费终端";
+	this.device_expire = "已欠费终端";
+	this.device_batchadd = "批量添加";
+	this.device_delSelDevice = "删除所选";
+	this.device_saleSelDevice = "销售所选";
+	this.device_name = "名称";
+	this.device_type = "终端类型";
+	this.device_chnCount = "通道数目";
+	this.device_simCard = "SIM卡号";
+	this.device_dateProduct = "出厂日期";
+	this.device_client = "客户";
+	this.device_add = "添加终端";
+	this.device_edit = "修改终端";
+	this.device_sale = "销售";
+	this.device_batchadd = "批量添加";
+	this.device_batchaddTitle = "批量添加终端";
+	this.device_unregAddTitle = "添加未注册终端";
+	this.device_labelName = "名称：";
+	this.device_labelIDNO = "编号：";
+	this.device_labelDevType = "终端类型：";
+	this.device_labelChnCount = "通道数目：";
+	this.device_labelSimCard = "SIM卡号：";
+	this.device_labelDateProduct = "出厂日期：";
+	this.device_labelBatchCount = "添加数目：";
+	this.device_errChnCount = "范围0-12";
+	this.device_errBatchCount = "范围2-200";
+	this.device_errSimCardRegx = "必须为数字，在1-32个字符之间";
+	this.device_batchTip = "按照编号递增方式批量添加！";
+	this.device_lableSale = "销售给：";
+	this.device_saleTitle = "销售终端";
+	this.device_saleok = "销售成功";
+	this.device_selectclient = "请选择客户";
+	this.device_sale = "销售";
+	this.device_resale = "转移";
+	this.device_errSelectClient = "请选择客户";
+	this.device_labelOwner = "所属客户：";
+	this.device_labelDevList = "终端列表：";
+	this.device_exportVehicleExcel = "导出车载终端至Excel";
+	this.device_exportMobileExcel = "导出人员终端至Excel";
+	this.device_importExcel = "从Excel导入";
+	this.device_labelImportType = "导入类型：";
+	this.device_labelImportExcel = "选择Excel文件：";
+	this.device_tipSelectExcelFile = "请选择Excel文件！";
+	this.device_labelVeicleFormat = "车载DVR格式：";
+	this.device_labelMobileFormat = "人员终端格式：";
+	this.device_veicleFormat = "序号、车牌号、帐号、通道、SIM卡、司机名称、司机电话、车辆品牌、车辆类型(第二行开始为数据)";
+	this.device_mobileFormat = "序号、姓名、账户、性别、联系电话、职务、身份证、住址、备注";
+	this.device_importFailed = "部分数据导入不成功！";
+	this.device_selectAddress2 = "使用设备地址2";
+	this.device_selectPayment = "启用缴费";
+	this.device_labelPayBegin = "合约开始日：";
+	this.device_labelPayPeriod = "合约期：";
+	this.device_labelPayEnd = "合约到期日：";
+	this.device_pay6month = "6个月";
+	this.device_pay12month = "12个月";
+	this.device_pay18month = "18个月";
+	this.device_pay24month = "24个月";
+	this.device_labelPayDelay = "宽限天数：";
+	this.device_labelPayStatus = "合约状态：";
+	this.device_labelPayMonth = "已付月数：";
+	this.device_labelPayOverDay = "欠费天数：";
+	this.device_labelStoDay = "云录制（天）：";
+	this.device_payStatusNormal = "正常";
+	this.device_payStatusAbnormal = "欠费";
+	this.device_errPayMonth = "范围 0 - 24 月";
+	this.device_errPayDelay = "范围 0 - 14 天";
+	this.device_payBegin = "合约开始日";
+	this.device_payMonth = "已付月数";
+	this.device_payPeriod = "合约期";
+	this.device_payStatus = "合约状态";
+	this.device_payOverDay = "欠费天数";
+	this.device_payEnable = "启用缴费";
+	this.device_exportExcel = "导出至Excel";
+		
+	//-------------------------------用户信息管理-----------------------------/
+	this.client_title = "客户管理";
+	this.client_labelClientCount = "客户总数：";
+	this.client_add = "添加客户";
+	this.client_edit = "编辑客户";
+	this.client_name = "名称";
+	this.client_account = "帐号";
+	this.client_linkman = "联系人";
+	this.client_telephone = "电话";
+	this.client_email = "邮件";
+	this.client_url = "网址";
+	this.client_labelName = "名称：";
+	this.client_labelAccount = "帐号：";
+	this.client_labelLinkman = "联系人：";
+	this.client_labelTelephone = "电话：";
+	this.client_labelEmail = "邮件：";
+	this.client_labelAddress = "地址：";
+	this.client_labelUrl = "网址：";
+	this.client_viewTitle = "查看客户";
+	this.client_deviceList = "终端列表";
+	this.client_clientList = "用户列表";
+	this.client_deviceCount = "终端总数:";
+	this.client_userCount = "客户总数:";
+	this.client_resetPwd = "重置密码";
+	this.client_resetPwdIng = "正在重置密码....";
+	this.client_defaultPwdtip = "默认密码：";
+	this.client_operatorTip = "客户账户即为企业管理员帐号，每个客户具有独立的终端和用户信息。请使用客户帐号登录WEB，在用户管理界面添加子用户。";
+		
+	//-------------------------------在线状态信息-------------------------------/
+	this.status_title = "在线状态";
+	this.status_device = "在线终端";
+	this.status_client = "在线客户";
+	this.status_unreg = "未注册终端";
+	this.status_labelDeviceCount = "在线终端数目：";
+	this.status_labelUnregCount = "未注册数目：";
+	this.status_deviceJingWei = "经纬度";
+	this.status_deviceGpsTime = "GPS时间";
+	this.status_address = "地址";
+	this.status_network = "网络";
+	this.status_deviceGWaySvr = "所在的网关服务器";
+	this.status_labelClientCount = "在线客户端数目：";
+	this.status_clientLoginTime = "登录时间";
+	this.status_clientType = "类型";
+	this.status_clientOwner = "所属客户";
+	this.status_clientUserSvr = "所在的用户服务器";
+	this.status_clientWindow = "C客户端";
+	this.status_clientWeb = "Web客户端";
+	this.status_clientIphone = "iPhone客户端";
+	this.status_show_position = "地图位置";
+	this.status_deviceProtocol = "协议类型";
+	this.status_deviceAudioCodec = "音频类型";
+	this.status_deviceDiskType = "存储类型";
+	
+	//-------------------------------服务器管理-------------------------------/
+	this.server_login = "登录服务器";
+	this.server_gateway = "网关服务器";
+	this.server_media = "流媒体服务器";
+	this.server_user = "用户服务器";
+	this.server_storage = "存储服务器";
+	this.server_total = "共";
+	this.server_count = "个";
+	this.server_labelName = "名称：";
+	this.server_labelIDNO = "编号：";
+	this.server_labelStatus = "服务器状态：";
+	this.server_labelLANAddr ="局域网地址：";
+	this.server_labelWLANDevice ="设备服务地址1（公网）：";
+	this.server_labelWLANDevice2 ="设备服务地址2（公网）：";
+	this.server_labelWifiDevice ="设备服务地址（WIFI）：";
+	this.server_labelPortDevice ="设备服务端口：";
+	this.server_labelWLANClient ="客户服务地址1（公网）：";
+	this.server_labelWLANClient2 ="客户服务地址2（公网）：";
+	this.server_labelPortClient ="客户服务端口：";
+	this.server_errIPAddress = "IP地址不合法";
+	this.server_errPort = "范围: 0-65535";
+	this.server_errPortEqual = "不得与设备服务端口相同";
+	this.server_name = "名称";
+	this.server_lanip = "局域网地址";
+	this.server_wifiDevice ="设备服务地址（WIFI）";
+	this.server_deviceIp = "设备地址1";
+	this.server_deviceIp2 = "设备地址2";
+	this.server_devicePort = "设备端口";
+	this.server_clientIp = "客户地址1";
+	this.server_clientIp2 = "客户地址2";
+	this.server_clientPort = "客户端口";
+	this.server_add = "新增服务器";
+	this.server_edit = "编辑服务器";
+	this.server_downStation = "下载站点";
+	this.server_down = "下载服务器";
+	this.server_stationPosition = "位置";
+	this.server_stationSsid = "SSID";
+	this.server_stationType = "区分方式";
+	this.server_stationByPosition = "地理位置";
+	this.server_stationBySsid = "SSID名称";
+	this.server_addStation = "添加下载站点";
+	this.server_stationEdit = "编辑下载站点";
+	this.server_labelSsid = "SSID：";
+	this.server_labelIp = "IP：";
+	this.server_stationTip = "通过SSID名称来区分的下载站点，必须保证站点的ssid名称唯一性";
+	this.server_selectStationTip = "请选择下载站点";
+	this.server_downStationNullTip = "不存在下载站点信息，请添加下载站点后再进行操作！";
+	this.server_labelDownStation = "下载站点：";
+	this.server_addStorageRelation = "添加存储关联";
+	this.server_editStorageRelation = "修改存储关联";
+	this.server_stoRelationTip = "提示：请配置存储关联，再配置存储计划";
+	this.server_labelStoServer = "当前的存储服务器：";
+	this.server_labelStoRelationCount = "已关联数目：";
+	this.server_relation = "终端关联";
+	this.server_tipServerAddress2 = "通常情况请将地址1和地址2保持一致";
+	this.server_offlineTimeoutError = "範圍: 30-1800秒";
+	this.server_labelOfflineTimeout = "設備離線時長(秒)："
+	
+	//-------------------------------系统用户日志查询-------------------------------/
+	this.log_queryTitle = "日志查询";
+	this.log_querySysUsr = "系统用户日志";
+	this.log_queryServer = "服务器日志";
+	
+	//-------------------------------广告消息管理-------------------------------/
+	this.adAndNews_title = "广告消息管理";
+	this.adAndNews_ad = "广告";
+	this.adAndNews_news = "消息通知";
+	this.adAndNews_delSelAdOrNews = "删除所选";
+	this.adAndNews_nameTitle = "标题";
+	this.adAndNews_releaseDate = "发布日期";
+	this.adAndNews_validity = "有效期限";
+	this.ad_validity = "有效期限：";
+	this.ad_add = "添加广告";
+	this.news_add = "添加消息通知";
+	this.ad_edit = "修改广告";
+	this.news_edit = "修改消息通知";
+	this.ad_title = "标题：";
+	this.ad_content = "内容：";
+	this.addPicture = "添加大图：";
+	this.errTitleRegex = "不得超过100个字符";
+	this.errNewsTitleRegex = "不得超过100个字符";
+	this.errNewsContentRegex = "不得超过300个字符";
+	this.errAdContentRegex = "不得超过1000个字符";
+	this.adAndNews_errValidity = "范围天数1-1000天";
+	this.errPicture = "请选择图片";
+	this.selectedImg = "选择图片";
+	this.pictureSize = "（建议图片分辨率1280*800，大小不超过1M，动态图片仅支持gif格式）";
+	//-------------------------------错误代码定义-------------------------------/
+	//登录部分错误代码
+	this.errLogin_UserNoExist = "用户帐号不存在！";
+	this.errLogin_PasswordError = "密码错误！";
+	this.errLogin_Expired = "用户帐号已过期！";
+	this.errLogin_Verify = "验证码错误！";
+	//输入部分
+	this.errStringRequire = "不得为空";
+	this.errNameRegex = "不得超过32个字符";
+	this.errIDNORegex = "必须为数字或者字母，在1-32个字符之间";
+	this.errAccountRegex = "必须为数字或者字母，在4-32个字符之间";
+	this.errSelectedRequired = "没有选择记录信息！";
+	this.errEmailFormat = "邮件格式不正确";
+	this.errUrlFormat = "网址格式不正确";
+	//系统部分
+	this.errSendRequired = "向服务器发送请求失败！";	
+	this.errUnkown = "未知错误！";
+	this.errException = "系统出现异常！";			//RET_EXCEPTION_ERROR	1
+	this.errSessionUnvalid = "会话已过期！";		//RET_SESSION_UNVALID	2
+	this.errTimeout = "请求出现超时";				//RET_REQUIRE_TIMEOUT	3
+	this.errExceptionRequire = "请求出现异常";		//RET_EXCEPTION_REQUIRE	4
+	this.errExceptionNetwork = "网络出现异常";			//RET_EXCEPTION_NETWORK	5
+	this.errQueryTimeFormat = "查询时间格式不正确！";	//RET_TIME_FROMAT_ERR	6
+	this.errQueryTimeRange = "查询的开始时间不得大于结束时间！";	//RET_TIME_RANGE_ERR	7
+	this.errRequireParam = "请求参数不正确！";			//RET_REQUIRE_PARAM  8
+	this.errServerNoExist = "服务器信息不存在！";		//RET_SERVER_NO_EXIST  9
+	this.errServerTypeErr = "服务器类型不正确！";		//RET_SERVER_TYPE_ERR = 10;	//服务器类型信息不正确
+	this.errIDNOExist = "编号已经被使用！";	//RET_IDNO_EXIST = 11;	//服务器编号已经被使用
+	this.errDeviceNoExist = "终端信息不存在！";	//RET_DEVICE_NO_EXIST = 12;	//设备信息不存在
+	this.errDeviceLimitErr = "终端数目不得超过系统可以管理的设备终端！";	//RET_DEVICE_LIMIT_ERR = 13;
+	this.errDeviceBatchIdnoErr = "终端编号后3位必须为数字，并且递增后还保持有效！";	//public static final int RET_DEVICE_BATCH_IDNO_ERR = 14;
+	this.errAccountExist = "帐号已经被使用！";		//	RET_ACCOUNT_EXIST = 15; 帐号已经被使用
+	this.errClientNoExist = "客户信息不存在！";	//RET_CLIENT_NO_EXIST = 16; 客户信息不存在
+	this.errClientHasDevice = "客户还有终端信息！无法删除！";	//RET_CLIENT_HAS_DEVICE = 17;	//客户还有设备信息！无法删除！
+	this.errClientHasUser = "客户还有子用户信息！无法删除！";	//RET_CLIENT_HAS_USER = 18;	//客户还有子用户信息！无法删除！
+	this.errOldPwd = "旧密码有误！";	//RET_OLD_PWD_ERROR = 19;	//旧密码有误！
+	this.errUserNoExist = "用户信息不存在！";	//RET_USER_NO_EXIST = 20;	//用户信息不存在
+	this.errRoleNoExist = "角色信息不存在！";	//RET_ROLE_NO_EXIST = 21;	//角色信息已经存在
+	this.errRoleNameExist = "角色名称已经被使用!";	//RET_ROLE_NAME_EXIST = 22;	//角色名称已经被使用
+	this.errRoleHasUsed = "还有用户使用此角色信息！无法删除！";	//RET_ROLE_HAS_USER = 23;	//还有用户使用此角色信息！无法删除！
+	this.errNoPrivilige = "用户无权限！";	//RET_NO_PRIVILIGE = 24;	//用户无权限
+	this.errVehicelNoExist = "终端信息不存在！";	//RET_VEHICLE_NO_EXIST = 25;	//车辆信息不存在
+	this.errGroupNoExist = "分组不存在！";	//RET_GROUP_NO_EXIST = 26;	//车辆分组不存在
+	this.errGroupHasUsed = "分组信息还在被占用（还存储子分组或者终端）！";	//RET_GROUP_HAS_USED = 27;	//车辆分组信息还在被占用（还存储子分组或者下级车辆）
+	this.errDbConnErr = "数据库连接出现异常！";	//RET_DB_CONN_ERR = 28;	//数据库连接出现异常
+	this.errNameExist = "名称已经被使用！";	//RET_NAME_EXIST = 29;	//名称已经被使用
+	this.errNoExist = "信息不存在！";	//RET_NO_EXIST = 30;		//信息不存在
+	this.errDownStationSsidExist = "下载站点SSID已经被使用！";//RET_DOWN_STATION_SSID_EXIST = 31;		//下载站点SSID已经被使用
+	this.errDownStationUsed = "下载站点还被使用（拥有下载服务器信息）！";//RET_DOWN_STATION_USED = 32;	//下载站点还被使用（拥有下载服务器信息）
+	this.errDownStationNoExist = "下载站点信息不存在！";	//RET_DOWN_STATION_NO_EXIST = 33;	//下载站点信息不存在
+	this.errGroupNameUsed = "同一分组下不允许存在相同名称的分组信息！";	//RET_GROUP_NAME_USED = 34;	//同一分组下不允许存在相同名称的分组信息
+	this.errDeviceHasRegister = "设备信息已经登记到系统中";	//RET_DEVICE_HAS_REGISTER = 35;	//设备信息已经登记到系统中
+	this.errServerNoSupport = "服务器不支持此功能";	//RET_SERVER_NO_SUPPORT = 36;	//服务器不支持此功能
+	this.errImageSize = "图片大小不能超过1M";	//RET_IMAGE_SIZE_ERR = 41;	//图片大小超过1M
+	this.errSImageType = "图片格式错误";	//RET_IMAGE_TYPE_ERR = 42;	//图片格式错误
+	this.errCompanyHasCompany = "公司还有子公司信息！无法删除！";//RET_COMPANY_HAS_COMPANY = 45;	//公司还有子公司信息！无法删除！
+	this.errUserDeactivated = "用户已停用";//RET_USER_DISABLED = 46;	//用户已停用
+	this.errDeviceInstalled = "设备已安装，无法删除！";//RET_INSTALLED = 47;	//设备已安装
+	this.errJobNumberExists = "工号已存在";//RET_JOBNUM_EXIST = 48;	//工号已存在
+	this.errSIMCardExists = "SIM卡号已存在";//RET_SIMCARD_EXIST = 49;	//SIM卡号已存在
+	this.errVehiTeamNotMove = "车队不能移动到父公司,只能向所在公司子公司移动";//RET_VEHITEAM_NOT_MOVE = 50; //车队不能移动到父公司
+	
+	
+	this.errorConnection = "连接错误";
+	this.pagestatInfo = "显示由  {from}  到  {to}  共  {total}  条记录";
+	this.of = "到";
+	this.find = "查找";
+	this.nomsg = "没有记录";
+	this.procmsg ="处理中，请稍候...";
+	this.pernumber = "每页数目";
+	this.pagefrom = "第";
+	this.pagetotal = "共";
+	
+	this.system_show_rule = "显示规则：";
+	this.system_seq_date_asc = "按添加时间升序";
+	this.system_seq_date_desc = "按添加时间降序";
+}
